05d24285138267b7e9590ebd28df9f72fce3fc45
Vadym Shakhraichuk <shakhraichuk@gmail.com>
2021-07-31 01:22:16 -0700

    Images Upload Form 1

--- server/routes/file.route.js
+++ server/routes/file.route.js
@@ -3,7 +3,7 @@ let mongoose = require('mongoose');
 let multer = require('multer');
 let router = express.Router();
 
-router.use('/uploads', express.static('uploads'));
+let File = require('../models/File');
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -15,17 +15,47 @@ const storage = multer.diskStorage({
   }
 })
 
-const upload = multer({ storage: storage }).single('profileImg')
+var upload = multer({
+    storage: storage,
+    fileFilter: (req, file, cb) => {
+        if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
+            cb(null, true);
+        } else {
+            cb(null, false);
+            return cb(new Error('File type not accepted (.png, .jpg, .jpeg)'));
+        }
+    }
+});
 
-router.post('/upload', (req, res) => {
-    upload(req, res, (err) => {
-      if (err) {
-        res.sendStatus(500);
-        console.log('error')
-      }
-      console.log('GOOD')
-      res.send(req.file);
+router.post('/upload', upload.array('image', 8), (req, res, next) => {
+    const reqFiles = [];
+
+    const url = req.protocol + '://' + req.get('host')
+
+    for (var i = 0; i < req.files.length; i++) {
+        reqFiles.push(url + '/public/' + req.files[i].filename)
+    }
+
+    const user = new File({
+        _id: new mongoose.Types.ObjectId(),
+        imagesArray: reqFiles
     });
-  });
+
+    user.save().then(result => {
+        res.status(201).json({
+            message: "Uploaded!",
+            userCreated: {
+                _id: result._id,
+                imagesArray: result.imagesArray
+            }
+        })
+    }).catch(err => {
+        console.log(err),
+            res.status(500).json({
+                error: err
+            });
+    })
+})
+
 
 module.exports = router;
